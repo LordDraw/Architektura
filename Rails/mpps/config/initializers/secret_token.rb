ca8eaa190e2ea5f22f59d8ba548a92642593529c
Pawel Karczewski <pawel.j.karczewski@gmail.com>
2012-10-23 12:09:35 +0200

    djfwefqwefedfh

---
+++ Rails/mpps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpps::Application.config.secret_token = '8a11751d7dda8c2ac8a46061c4d7a8f9fc873b7c2ae19a02216d16554eabb277f7f60c04c92e5236156753a7fb2c592976cb4da1816bc2e67c20035bba0b093a'
