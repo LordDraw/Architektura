a9d6dddd402af26689debb33f27cd144e30e5c68
Pawel Karczewski <pawel.j.karczewski@gmail.com>
2012-10-30 12:11:14 +0100

    fortunka

---
+++ library/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '66d30d761423e086657b0c126429ffcb431f25b61cf950dcabce56f340b1831dbe758400e3b3af9b406ede41a077a2f28d6589381c5b40577944475438280e57'
